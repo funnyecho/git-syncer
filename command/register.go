1a08d9a69b6ad8a168d63c0171d2335a5f4553be
SamHwang1990 <samhwang1990@gmail.com>
2021-01-18 17:51:36 +0800

    feat: register `catchup`, `push` and `config` cmd

--- command/register.go
+++ command/register.go
@@ -1,6 +1,9 @@
 package command
 
 import (
+	"github.com/funnyecho/git-syncer/command/catchup"
+	"github.com/funnyecho/git-syncer/command/config"
+	"github.com/funnyecho/git-syncer/command/push"
 	"github.com/funnyecho/git-syncer/command/setup"
 	"github.com/mitchellh/cli"
 )
@@ -8,9 +11,10 @@ import (
 // Register all syner command factories
 func Register() map[string]cli.CommandFactory {
 	return map[string]cli.CommandFactory{
-		"setup": setup.Factory,
-		//"catchup":      catchup.Factory,
-		//"push":         push.Factory,
+		"setup":   setup.Factory,
+		"catchup": catchup.Factory,
+		"push":    push.Factory,
+		"config":  config.Factory,
 		//"add-scope":    add_scope.Factory,
 		//"remove-scope": remove_scope.Factory,
 		//"show":         show.Factory,
